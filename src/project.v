df8addeaa6b920c39533520ae8b8aff19fccce62
Sachinatgithubb <166131393+Sachinatgithubb@users.noreply.github.com>
2024-04-10 15:22:58 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,14 +20,12 @@ module tt_um_PWM_Generator_Verilog (
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out = 0;
     assign uio_oe  = 0;
-    wire increase_duty;
-    wire decrease_duty;
-    wire PWM_OUT;
-
+  
     assign increase_duty=ui_in[0];
     assign decrease_duty=ui_in[1];
-    //assign ui_in[7:2]=0;
     assign PWM_OUT=uo_out[0];
+    
+    //assign ui_in[7:2]=0;    
     //assign ui_out[7:1]=0;
     
  wire slow_clk_enable; // slow clock enable signal for debouncing FFs
