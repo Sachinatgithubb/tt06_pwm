e7e10b191512b93d839040a12158227b1b0a9972
Sachinatgithubb <166131393+Sachinatgithubb@users.noreply.github.com>
2024-04-10 15:49:48 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,6 +20,8 @@ module tt_um_PWM_Generator_Verilog (
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out = 0;
     assign uio_oe  = 0;
+    assign rst_n = 0;
+    
   
     //assign increase_duty=ui_in[0];
     //assign ui_in[1]=ui_in[1];
